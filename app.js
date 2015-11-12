ecb41ac74a68ea0617d3b3f2dfa7179747236c97
Ducas Francis <ducas@ducasfrancis.com>
2015-11-12 18:02:25 +1100

    add producer to publish random stock messages

---
+++ app.js
@@ -0,0 +1,70 @@
+var kafka = require('kafka-node'),
+    Promise = require('promise'),
+    config = require('config-node')(),
+    util = require('util'),
+
+    clientId = config.clientId,
+
+    client = new kafka.Client(config.kafka.zkConnect, clientId),
+    producer = new kafka.HighLevelProducer(client, { requireAcks: 1, ackTimeoutMs: 100 }),
+
+    producerInterval,
+
+    exit = function () {
+        clearInterval(producerInterval);
+        producer.close(function () {
+            process.exit();
+        });
+    },
+
+    stocks = [{
+        name: 'AXJO',
+        price: 5122.60
+    }, {
+        name: 'AORD',
+        price: 5181.10
+    }, {
+        name: 'AAPL',
+        price: 116.12
+    }, {
+        name: 'GOOG',
+        price: 735.40
+    }, {
+        name: 'YHOO',
+        price: 33.38
+    }];
+
+producer.on('ready', function () {
+    producer.createTopics([config.kafka.topic], false, function () {});
+
+    producerInterval = setInterval(function () {
+
+        var index = Math.floor(Math.random() * stocks.length),
+            stock = stocks[index],
+            delta = Math.floor(Math.random() * stock.price * 10) / 100,
+            multiplier = Math.random() < 0.5 ? -1 : 1;
+
+        stock.price = Math.floor((stock.price + multiplier * delta) * 100) / 100;
+        stocks[index] = stock;
+
+        console.log(stock);
+
+        producer.send(
+            [{
+                topic: config.kafka.topic,
+                messages: JSON.stringify(stock),
+                attributes: 2
+            }], function (e) {
+                if (e) {
+                    console.error("Error sending message to destination: " + e);
+                }
+            });
+
+    }, 100);
+});
+
+process.on('SIGINT', function () {
+    console.log('Shutting down...');
+    exit();
+});
+
